0384398ee97fc0bef53c61fe0b2b7e2ee9efdc7f
CHUA Chee Wee <github@chuacw.ath.cx>
2013-12-17 16:57:00 +0800

    Initial check-in

---
+++ RemoveIIS7ServerHeader/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+
+namespace RemoveIIS7ServerHeader
+{
+    public class CustomHeaderModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        public void Dispose() { }
+
+        void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            //HttpContext.Current.Response.Headers.Set("Server", "Box of Bolts");
+            HttpContext.Current.Response.Headers.Remove("Server");
+            HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+        }
+    }
+}
---
+++ RemoveIIS7ServerHeader/readme.txt
@@ -0,0 +1,13 @@
+﻿Add the following line to /configuration/system.WebServer.modules
+
+<add name="CustomHeaderModule" type="RemoveIIS7ServerHeader.CustomHeaderModule" />
+
+like so...
+
+<configuration>
+  <system.webServer>
+    <modules>
+      <add name="CustomHeaderModule" type="RemoveIIS7ServerHeader.CustomHeaderModule" />
+    </modules>
+  </system.webServer>
+</configuration>	
